1a50feeff641900b5909c673c8b6ba6430800a4f
Oleg Smushkevich <olegs777@bezeqint.net>
2019-08-04 20:32:34 +0300

    ok

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View, Alert } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Alert,
+  TextInput,
+  TouchableHighlight
+} from "react-native";
 import Header from "../sections/Header";
 
 export default class Contact extends Component {
@@ -20,20 +27,38 @@ export default class Contact extends Component {
   sendMessage = () => {
     const { msg, name } = this.state;
     Alert.alert(name, msg);
-    this.props.navigation.goBack("HomeRT");
+    this.props.navigation.goBack();
   };
   render() {
-    console.log("this.props", this.props);
-    const { name, msg, text } = this.state;
+    const { name, msg, email } = this.state;
+
     return (
       <View style={styles.container}>
         <Header message="Press to Login" />
         <Text style={styles.heading}> Contact us</Text>
         <TextInput
-          style={styles.input}
+          style={styles.inputs}
           onChangeText={text => this.setState({ name: text })}
           value={name}
         />
+        <TextInput
+          style={styles.multiInput}
+          onChangeText={text => this.setState({ msg: text })}
+          value={msg}
+          multiline={true}
+          numberOfLines={4}
+        />
+        <TextInput
+          style={styles.inputs}
+          onChangeText={text => this.setState({ email: text })}
+          value={email}
+        />
+        <TouchableHighlight onPress={this.sendMessage} underlayColor="#31e981">
+          <Text style={styles.buttons}>Send Message</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.clearFields} underlayColor="#31e981">
+          <Text style={styles.buttons}>Clear Fields</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -41,6 +66,30 @@ export default class Contact extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    alignItems: "center",
+    paddingBottom: "50%"
+  },
+  heading: {
+    flex: 1,
+    fontSize: 16
+  },
+  inputs: {
+    flex: 1,
+    width: "80%",
+    padding: 5,
+    borderBottomWidth: 1,
+    borderColor: "gray"
+  },
+  multiInput: {
+    flex: 2,
+    width: "80%",
+    paddingTop: 5,
+    borderBottomWidth: 1,
+    borderColor: "gray"
+  },
+  buttons: {
+    marginTop: 15,
+    fontSize: 16
   }
 });
