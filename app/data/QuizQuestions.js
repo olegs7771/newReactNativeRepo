eb3d62bd12c92b0b4fe1bc72cdaaa6e270874e77
Oleg Smushkevich <olegs777@bezeqint.net>
2019-08-10 17:53:22 +0300

    ok

---
+++ app/data/QuizQuestions.js
@@ -0,0 +1,42 @@
+export const QuizData = {
+  questions: [
+    {
+      question:
+        "What language do you use with libraries such as React and Angular?",
+      answer1: "PHP",
+      answer2: "JAVA",
+      answer3: "JavaScript",
+      answer4: "Pyton",
+      correctAnswer: "answer3",
+      key: "1A"
+    },
+    {
+      question: "What markup language to build web page?",
+      answer1: "HTML",
+      answer2: "JAVA",
+      answer3: "Pyton",
+      answer4: "C",
+      correctAnswer: "answer1",
+      key: "1b"
+    },
+    {
+      question:
+        "Which one of these ways is a way  to store  multiple valuables in a single variable?",
+      answer1: "string",
+      answer2: "array",
+      answer3: "number",
+      answer4: "boolean",
+      correctAnswer: "answer2",
+      key: "1c"
+    },
+    {
+      question: "How you indicate values are not equal in JavaScript",
+      answer1: "===",
+      answer2: "=>",
+      answer3: "!==",
+      answer4: "<=",
+      correctAnswer: "answer3",
+      key: "1d"
+    }
+  ]
+};
